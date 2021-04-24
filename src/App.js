e29346f54f0682963f6dbcb7e83c45000f8b2c6c
rokibulislam <rrcucse.11@gmail.com>
2021-04-24 06:07:35 +0600

    Create TodoList Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
+import React, { useState } from 'react'
+import TodoList from './components/Todos/TodoList'
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [ todos, setTodos ] = useState([
+      {
+          id: 1,
+          text: 'Todo One',
+          done: true
+      },
+
+      {
+          id: 2,
+          text: 'Todo Two',
+          done: false
+      },
+
+      {
+        id: 3,
+        text: 'Todo Three',
+        done: true
+      },
+
+      {
+        id: 4,
+        text: 'Todo Four',
+        done: false
+      },
+
+      {
+        id: 5,
+        text: 'Todo Five',
+        done: true
+      }
+  ]);
+
+  const handleClick = todo => {
+
+      const mapTodos = todos.map(item => {
+          if (item.id === todo.id) {
+            return {
+              ...item,
+              done: true
+            };
+          }
+          return item;
+      });
+
+      setTodos(mapTodos);
+  }
+
   return (
     <div className="App">
-        React App
+        <TodoList items={todos} onItemClick={handleClick} />
     </div>
   );
 }
---
+++ src/components/Todos/TodoList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./TodoList.scss";
+
+const TodoList = ({ items, onItemClick }) => {
+  
+    const handleClick = (e, todo) => {
+        e.preventDefault();
+        if (!todo.done) {
+            onItemClick(todo);
+        }
+    };
+
+    return ( 
+        <>
+            <table className="todo-list">
+                <caption>My Todos</caption>
+                <tbody>
+                {items.length > 0 ? items.map( todo => (
+                    <tr className={ 'todo-item' + (!todo.done ? ' going' : ' done') } onClick={e => handleClick(e, todo)} key={todo.id}>
+                        <td className="todo-text">{ todo.text }</td>
+                        <td className={'todo-' + (!todo.done ? 'done' : 'going')}>{ todo.done ? 'Done' : 'going' }</td>
+                    </tr>
+                )) : <tr className="todo-not-found"> <td> No todo found </td> </tr>}
+                </tbody>
+            </table>
+        </>
+    );
+}
+ 
+export default TodoList;
